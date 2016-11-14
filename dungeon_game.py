d9e5c4bc91c8d729b50c0db5d62ff236faf92d43
Mateusz Siga <sigamateusz@gmail.com>
2016-11-14 12:35:39 +0100

    add getch()

---
+++ dungeon_game.py
@@ -0,0 +1,15 @@
+def getch():
+    import sys
+    import tty
+    import termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+x = getch()
+print(x)
