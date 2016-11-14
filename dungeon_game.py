e6a5fa27f468ae632484561433945dbbf4a73fae
Mateusz Siga <sigamateusz@gmail.com>
2016-11-14 15:04:12 +0100

    done user_move()

--- dungeon_game.py
+++ dungeon_game.py
@@ -41,17 +41,40 @@ def display_gameboard(table):
         cprint(' '.join(table[row]), 'magenta')
 
 
-def user_move(user_position):
-    print(user_position[0][0])
+def user_move(x, y, user_position, *args):
+    table = gameboard(x, y)
+    x_user = user_position[0]
+    y_user = user_position[1]
+    move = getch()
+    if move == 'd':
+        x_user += 1
+        if table[x_user][y_user] == '#':
+            x_user -= 1
+    elif move == 'a':
+        x_user -= 1
+        if table[x_user][y_user] == '#':
+            x_user += 1
+    elif move == 'w':
+        y_user -= 1
+        if table[x_user][y_user] == '#':
+            y_user += 1
+    elif move == 's':
+        y_user += 1
+        if table[x_user][y_user] == '#':
+            y_user -= 1
+    user_position[0] = x_user
+    user_position[1] = y_user
     return user_position
 
 
 def main():
-    user_coordinates = [3, 1]
+    user_coordinates = [1, 1]
+    wide_gameboard = 20
+    height_gameboard = 20
     while True:
-        display_gameboard(gameboard(10, 20, user_coordinates))
-        #  user_move(user_coordinates)
-        time.sleep(0.5)
+        display_gameboard(gameboard(wide_gameboard, height_gameboard, user_coordinates))
+        user_move(wide_gameboard, height_gameboard, user_coordinates)
+        time.sleep(0.1)
 
 if __name__ == '__main__':
     main()
