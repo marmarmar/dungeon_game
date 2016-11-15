0c8456658451067b89d95e6a0779e6f6874177d2
Marek Stopka <keramakpots@gmail.com>
2016-11-15 10:56:54 +0100

    Else

--- dungeon_game.py
+++ dungeon_game.py
@@ -27,12 +27,12 @@ def gameboard(x=5, y=5, user_position=[1, 1]):
         table.append([])
         for column in range(y):
             if row == 0 or row == x-1 or column == 0 or column == y-1 or \
-                    (row > 15 and row < 16 and column > 8 and column < 13) or \
-                    (row > 7 and row < 10 and column > 5 and column < 8) or \
-                    (row > 24 and row  < 27 and column > 30 and column < 35) or \
-                    (row > 5 and row < 10 and column > 20 and column < 30) or \
-                    (row > 12 and row < 15 and column > 20 and column < 25) or \
-                    (row < 2 and column > 12 and column < 17) or \
+                    (row > 5 and row < 7 and column > 3 and column < 8) or \
+                    (row > 22 and row < 28 and column > 22 and column < 33) or \
+                    (row > 13 and row  < 17 and column > 3 and column < 5) or \
+                    (row > 5 and row < 10 and column > 34 and column < 37) or \
+                    (row > 12 and row < 15 and column > 2 and column < 6) or \
+                    (row < 4 and column > 12 and column < 17) or \
                     (row > 27 and row < 33 and column > 2 and column < 9):
                 table[row].append('#')
             elif row == x_user and column == y_user:
