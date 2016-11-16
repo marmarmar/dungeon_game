0d81e468c409f1d00c2926c01ae7304d0a002e7c
Marek Stopka <keramakpots@gmail.com>
2016-11-16 20:43:20 +0100

    drinking bottle to get drunk

--- drunk.py
+++ drunk.py
@@ -10,5 +10,7 @@ def print_drunk():
     cprint("\t\t        ╲╲╲╲╲┫╰╯┣╯╱╱╱╱╱╱╱", 'green', attrs=['bold'])
     cprint("\t\t        ┈┈┈┈┈╰┳┳╯┈┈┈┈┈┈┈┈", 'green', attrs=['bold'])
 
-    print("Time for a rest. Go back to your journey, when you ready.")
+    print("Time for a rest. )
+    print("But who would save the world without you?")
+    print("Go back to your journey, when you ready.")
     print("(press any key to exit)")
